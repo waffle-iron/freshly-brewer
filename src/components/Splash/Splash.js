537b8ee6d185647de0249c40a594d4e6383c2373
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2017-04-22 21:19:03 -0400

    initial splash page

---
+++ src/components/Splash/Splash.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Splash.css';
+
+export default class Splash extends Component {
+  render() {
+    return (
+      <div className="Splash">
+        <div className="SplashFooter">
+          <h1 className="SplashHeader">Coming soon</h1>
+        </div>
+      </div>
+    );
+  }
+}
