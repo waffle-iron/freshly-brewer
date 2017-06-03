bd40a5c497b5ce7ee47cafab6332739884c1d272
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2017-06-03 15:16:36 -0400

    add react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 import Splash from './components/Splash';
 import Home from './containers/Home';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Home />
-      </div>
-    );
-  }
-}
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <Route exact path="/" component={Home}/>
+      <Route path="/splash" component={Splash}/>
+    </div>
+  </Router>
+)
 
 export default App;
